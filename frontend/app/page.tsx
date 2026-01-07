ddd5f734f6ec62b3991f9f2ba874cbd6116113ae
Niyati Shetty <niyatishetty280@gmail.com>
2026-01-08 00:44:34 +0530

    setup canvas to draw and isDrawMode toggle button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,16 @@
-import {useRef, useEffect} from "react";
+import {useRef, useEffect, useState} from "react";
 
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const [isDrawMode, setIsDrawMode] = useState(false);
+
+  const isDrawingRef = useRef(false);
+
+  const lastPointerRef = useRef<{x: number; y: number} | null>(null);
+
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
 
   useEffect(() => {
     const video = videoRef.current;
@@ -15,6 +23,15 @@ export default function Home() {
 
       canvas.width = rect.width;
       canvas.height = rect.height;
+
+      const ctx = canvas.getContext("2d");
+      if(!ctx) return;
+
+      ctx.lineWidth = 3;
+      ctx.lineCap = "round";
+      ctx.strokeStyle = "red";
+
+      ctxRef.current = ctx;
     };
 
     video.addEventListener("loadedmetadata", handleLoadedMetaData);
@@ -40,8 +57,13 @@ export default function Home() {
             top: 0,
             left: 0,
             border: "1px solid red",
+            pointerEvents: isDrawMode ? "auto" : "none",
           }}></canvas>
       </div>
+
+      <button onClick={() => setIsDrawMode((prev) => !prev)}>
+        {isDrawMode ? "Disable Draw Mode" : "Enable Draw Mode"}
+      </button>
     </div>
   );
 }
